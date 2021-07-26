4447c7ed217d376f12e96e95f09c311bf38a67d3
OolongJohnson <83778844+OolongJohnson@users.noreply.github.com>
2021-07-26 11:45:16 +0200

    .hex uploaded

--- main.ts
+++ main.ts
@@ -1,3 +1,6 @@
-basic.forever(function () {
-	
+input.onButtonPressed(Button.A, function () {
+    music.playTone(262, 1)
+})
+input.onButtonPressed(Button.B, function () {
+    music.startMelody(music.builtInMelody(Melodies.Dadadadum), MelodyOptions.Once)
 })
